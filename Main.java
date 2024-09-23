45416c65ca793720bacbd076ac5cb3d64be1a0e2
ValeZh <128028729+ValeZh@users.noreply.github.com>
2024-09-23 21:38:37 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-
-        String text = "someone cat organisation";
-
-        System.out.println("Result: " + text.replaceAll("\\B\\w", "").replaceAll("\\s+", "").toUpperCase());
-    }
-}
